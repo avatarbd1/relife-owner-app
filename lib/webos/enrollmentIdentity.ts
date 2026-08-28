b088036617ee8d28d7daf24eb5d64513ffe8e3eb
avatarbd1 <163557596+avatarbd1@users.noreply.github.com>
2026-08-28 21:25:00 +0600

    Fail closed on unscoped staff enrollment

--- lib/webos/enrollmentIdentity.ts
+++ lib/webos/enrollmentIdentity.ts
@@ -2,7 +2,7 @@ import "server-only";
 
 import { readStaffEnrollmentToken, type StaffEnrollmentClaims } from "@/lib/staffEnrollment";
 import { listPasskeysForStaff } from "@/lib/webauthn";
-import { getActiveWebStaffById, toAccessContext, type WebStaffIdentity } from "@/lib/webos/staffDirectory";
+import { toAccessContext, type WebStaffIdentity } from "@/lib/webos/staffDirectory";
 import { getTenantScopedWebStaffIdentity } from "@/lib/webos/tenantStaffDirectory";
 
 export interface EnrollmentIdentity {
@@ -17,13 +17,10 @@ export async function getEnrollmentIdentity(
   const claims = readStaffEnrollmentToken(token);
   if (!claims) throw new Error("STAFF_ENROLLMENT_INVALID");
 
-  const hasTenantScope = Boolean(claims.organizationId && claims.clinicId);
-  const identity = hasTenantScope
-    ? await getTenantScopedWebStaffIdentity(claims.staffId, {
-        organizationId: claims.organizationId!,
-        clinicId: claims.clinicId!,
-      })
-    : await getActiveWebStaffById(claims.staffId);
+  const identity = await getTenantScopedWebStaffIdentity(claims.staffId, {
+    organizationId: claims.organizationId,
+    clinicId: claims.clinicId,
+  });
   if (!identity || !toAccessContext(identity)) {
     throw new Error("STAFF_ENROLLMENT_ACCESS_DENIED");
   }
